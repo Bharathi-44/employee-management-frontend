30ead2b2c6080227d37ddae7218ff9612c37f9c9
Bharathi-44 <bharathi.k.2000m@gmail.com>
2023-02-16 23:51:57 +0530

    second

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import UpdateEmployeeComponent from "./components/UpdateEmployeeComponent";
 import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
 
@@ -15,11 +15,25 @@ function App() {
         <div className="container pt-3">
           <Routes>
             <Route exact path="/" element={<ListEmployeeComponent />}></Route>
-            <Route path="/employees" element={<ListEmployeeComponent />}></Route>
-            <Route exact path="/add-employee" element={<CreateEmployeeComponent />}></Route>
-            <Route exact path="/update-employee/:id" element={<UpdateEmployeeComponent />}></Route> 
-            <Route exact path="/view-employee/:id" element={<ViewEmployeeComponent />}></Route> 
-
+            <Route
+              path="/employees"
+              element={<ListEmployeeComponent />}
+            ></Route>
+            <Route
+              exact
+              path="/add-employee"
+              element={<CreateEmployeeComponent />}
+            ></Route>
+            <Route
+              exact
+              path="/update-employee/:id"
+              element={<UpdateEmployeeComponent />}
+            ></Route>
+            <Route
+              exact
+              path="/view-employee/:id"
+              element={<ViewEmployeeComponent />}
+            ></Route>
           </Routes>
         </div>
         <FooterComponent />
